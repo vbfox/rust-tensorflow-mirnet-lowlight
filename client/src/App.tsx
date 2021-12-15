24638ea1322a3ff6e5cc857b237bdfa276014d1d
Julien Roncaglia <julien@roncaglia.fr>
2021-12-15 17:24:34 +0100

    Basic working web UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,99 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import SyncLoader from "react-spinners/SyncLoader";
 
-import logo from "./logo.svg";
 import "./App.css";
 
-function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+function createBlobUrl(blob: Blob) {
+  return (globalThis.webkitURL || globalThis.URL).createObjectURL(blob);
+}
+
+function revokeBlobUrl(url: string) {
+  return (globalThis.webkitURL || globalThis.URL).revokeObjectURL(url);
+}
+
+function useBlobUrlState(): [
+  string | undefined,
+  React.Dispatch<Blob | undefined>
+] {
+  const [state, setState] = useState<string | undefined>(undefined);
+  const setBlob = useCallback(
+    (value: Blob | undefined) => {
+      if (state !== undefined) {
+        revokeBlobUrl(state);
+      }
+
+      if (value !== undefined) {
+        setState(createBlobUrl(value));
+      } else {
+        setState(undefined);
+      }
+    },
+    [state, setState]
+  );
+
+  return [state, setBlob];
+}
+
+function Dropzone() {
+  const [displayUrl, setInputBlob] = useBlobUrlState();
+  const [outputUrl, setOutputBlob] = useBlobUrlState();
+  const [working, setWorking] = useState(true);
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      if (acceptedFiles.length === 0) {
+        return;
+      }
+
+      const file = acceptedFiles[0];
+      setInputBlob(file);
+      setOutputBlob(undefined);
+      setWorking(true);
+
+      var data = new FormData();
+      data.append("input", file);
+
+      fetch("http://127.0.0.1:3001/run", {
+        method: "POST",
+        body: data,
+      })
+        .then((r) => r.blob())
+        .then((blob) => {
+          setOutputBlob(blob);
+          setWorking(false);
+        })
+        .catch(() => {
+          setWorking(false);
+        });
+    },
+    [setInputBlob, setOutputBlob]
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: ["image/png", "image/jpeg"],
+  });
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="Drop">
       <input {...getInputProps()} />
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
       )}
+      <div className="images">
+        <div className="input">
+          {working && (
+            <div className="loader">
+              <SyncLoader color="#0262c8" />
+            </div>
+          )}
+          {displayUrl && <img src={displayUrl} alt="" />}
+        </div>
+        <div className="output">
+          {outputUrl && <img src={outputUrl} alt="" />}
+        </div>
+      </div>
     </div>
   );
 }
@@ -25,20 +101,7 @@ function MyDropzone() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropzone />
     </div>
   );
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,16 @@
+use actix_cors::Cors;
+use actix_multipart::Multipart;
+use actix_web::HttpResponse;
+use actix_web::http::StatusCode;
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use anyhow::Context;
 use anyhow::Result as AnyResult;
+use futures::{StreamExt, TryStreamExt};
 use image::io::Reader as ImageReader;
-use image::GenericImageView;
+use image::{GenericImageView, EncodableLayout, ImageEncoder};
+use image::png::PngEncoder;
+use std::fs::OpenOptions;
+use std::io::{self, Cursor, Write, BufWriter};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -33,13 +41,75 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
-async fn server() -> AnyResult<()> {
+async fn get_input_bytes(mut payload: Multipart) -> Result<Vec<u8>, actix_web::Error> {
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        if field.name() == "input" {
+            let mut bytes: Vec<u8> = Vec::new();
+
+            while let Some(chunk) = field.next().await {
+                let data = chunk?;
+                println!("Writing {} bytes", data.len());
+                bytes.write_all(&data)?;
+            }
+
+            println!("Found input with {} bytes", bytes.len());
+
+            std::fs::File::create("input.png").unwrap()
+                .write_all(&bytes).unwrap();
+
+            return Ok(bytes);
+        }
+    }
+
+    Err(actix_web::error::ErrorBadRequest("Field not found"))
+}
+
+async fn save_file(payload: Multipart) -> Result<HttpResponse, actix_web::Error> {
+    let input_bytes = get_input_bytes(payload).await?;
+    let input_image = ImageReader::new(Cursor::new(input_bytes))
+        .with_guessed_format()
+        .map_err(|e| actix_web::error::ErrorBadRequest(format!("Unable to guess format: {:?}", e)))?
+        .decode()
+        .map_err(|e| actix_web::error::ErrorBadRequest(format!("Invalid image: {:?}", e)))?;
+    let input_tensor = image_to_tensor(&input_image);
+    drop(input_image);
+    let model = MirnetModel::new("model").map_err(|e| {
+        actix_web::error::ErrorInternalServerError(format!("Can't initialize model: {:?}", e))
+    })?;
+
+    let output_tensor = model.run(&input_tensor).map_err(|e| {
+        actix_web::error::ErrorInternalServerError(format!("Error running model: {:?}", e))
+    })?;
+    let output_image = tensor_to_image(&output_tensor).map_err(|e| {
+        actix_web::error::ErrorInternalServerError(format!("Can't convert output: {:?}", e))
+    })?;
+
+    let mut output_writerx = Vec::<u8>::new();
+    PngEncoder::new(&mut output_writerx).write_image(output_image.as_bytes(),
+        output_image.width(),
+        output_image.height(),
+        image::ColorType::Rgb8).map_err(|e| {
+            actix_web::error::ErrorInternalServerError(format!("Can't encode output: {:?}", e))
+        })?;
+
+    Ok(HttpResponse::build(StatusCode::OK)
+            .content_type("image/png")
+            .body(output_writerx))
+}
+
+async fn server(port: u16) -> AnyResult<()> {
+    println!("Serving on 127.0.0.1:{}", port);
+
     HttpServer::new(|| {
+        let cors = Cors::permissive();
+
         App::new()
+            .wrap(cors)
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
+            .route("/run", web::post().to(save_file))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(format!("127.0.0.1:{}", port))?
     .run()
     .await?;
 
@@ -52,7 +122,7 @@ async fn main() -> AnyResult<()> {
     if let Some(path) = opt.input {
         single_file(path)?;
     } else {
-        server().await?;
+        server(3001).await?;
     }
 
     Ok(())
