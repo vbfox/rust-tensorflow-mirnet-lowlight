a620802e18e23b3fef327ecd6bcd52692c77b0d3
Julien Roncaglia <julien@roncaglia.fr>
2021-12-15 15:31:35 +0100

    Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
